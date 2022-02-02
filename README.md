f25316b481a7a3791ce665417b8ef4ffa4581961
Lucas Zacchi <zacchilucasm@gmail.com>
2022-02-02 15:36:53 -0300

    Add caching to requests; add tests

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 This is a simple API that gets information from the publicly available VIA Rail's live map of passenger trains in the Windsor-Québec City corridor.
 
-The information is available at [https://tsimobile.viarail.ca/data/allData.json]
+The information is available at https://tsimobile.viarail.ca/data/allData.json
--- app/app.py
+++ app/app.py
@@ -2,28 +2,34 @@ from typing import Any
 from flask import Flask
 from markupsafe import escape  # preventhtml injection
 
+import requests
+import requests_cache
 import urllib.request, json
 
 app = Flask(__name__)
 
-
-def get_train_info() -> Any:
-    with urllib.request.urlopen(
-        "https://tsimobile.viarail.ca/data/allData.json"
-    ) as url:
-        train_info = json.loads(url.read().decode())
-
-    return train_info
+requests_cache.install_cache("trains_cache", expire_after=300)
 
 
-@app.route("/train-info")
-def display_train_info() -> Any:
-    train_info = get_train_info()
-    return train_info
+def get_trains_info() -> Any:
+    url = "https://tsimobile.viarail.ca/data/allData.json"
+
+    with requests.Session() as session:
+        response = session.get(url)
+        trains_info = response.json()
+        print(response.from_cache)
+
+    return trains_info
 
 
-@app.route("/train-info/<train_number>")
+@app.route("/trains-info")
+def display_trains_info() -> Any:
+    trains_info = get_trains_info()
+    return trains_info
+
+
+@app.route("/trains-info/<train_number>")
 def display_train(train_number: str) -> Any:
-    train_info = get_train_info()
+    train_info = get_trains_info()
 
-    return train_info[train_number]
+    return train_info[escape(train_number)]
